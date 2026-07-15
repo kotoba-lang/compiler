a660aedf1da9f0677b87b860897127fbcebb210b
Jun Kawasaki <jun784@gmail.com>
2026-07-15 18:46:02 +0900

    feat(aiueos): export storage read validators

--- src/kotoba/compiler/packaging/elf64.clj
+++ src/kotoba/compiler/packaging/elf64.clj
@@ -13,7 +13,9 @@
   {journal-entry {:arity 4 :symbol "kotoba_aiueos_journal_plan"}
    'aiueos-fnv1a {:arity 2 :symbol "kotoba_aiueos_fnv1a"}
    'aiueos-journal-record-valid {:arity 2 :symbol "kotoba_aiueos_journal_record_valid"}
-   'aiueos-object-transaction-valid {:arity 2 :symbol "kotoba_aiueos_object_transaction_valid"}})
+   'aiueos-object-transaction-valid {:arity 2 :symbol "kotoba_aiueos_object_transaction_valid"}
+   'aiueos-mutable-object-valid {:arity 5 :symbol "kotoba_aiueos_mutable_object_valid"}
+   'aiueos-superblock-valid {:arity 2 :symbol "kotoba_aiueos_superblock_valid"}})
 
 (defn- le [n width]
   (mapv #(bit-and (unsigned-bit-shift-right (long n) (* 8 %)) 0xff)
@@ -146,7 +148,8 @@
     ;; lea r9,[rip+.data] (relocated); optionally replenish bounded-memory
     ;; fuel; sub rsp,8; call local Kotoba entry; add rsp,8; ret.
     (let [bounded-memory? (contains? '#{aiueos-fnv1a aiueos-journal-record-valid
-                                        aiueos-object-transaction-valid} object-entry)
+                                        aiueos-object-transaction-valid aiueos-mutable-object-valid
+                                        aiueos-superblock-valid} object-entry)
           replenish (when bounded-memory?
                       [0x49 0xc7 0x41 0x08 0x00 0x04 0x00 0x00]) ; [r9+8]=1024
           wrapper (vec (concat [0x4c 0x8d 0x0d 0 0 0 0] replenish
