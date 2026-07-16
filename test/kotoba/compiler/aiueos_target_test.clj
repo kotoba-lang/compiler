270cc8955f9dd8a8742124da3d878149c947b36f
Jun Kawasaki <jun784@gmail.com>
2026-07-16 16:01:08 +0900

    Export aiueos capability mutation planner

--- test/kotoba/compiler/aiueos_target_test.clj
+++ test/kotoba/compiler/aiueos_target_test.clj
@@ -342,6 +342,12 @@
     (is (= "kotoba_aiueos_service_task_transition" (:export object)))
     (is (empty? (:imports object)))))
 
+(deftest kernel-target-exports-capability-mutation-plan
+  (let [source "(defn aiueos-capability-mutation-plan [action generation type state request] action) (defn main [] 0)"
+        {:keys [object]} (compiler/compile-source source :x86_64-aiueos-kernel-v1)]
+    (is (= "kotoba_aiueos_capability_mutation_plan" (:export object)))
+    (is (empty? (:imports object)))))
+
 (deftest bounded-kernel-memory-is-rejected-for-host-targets
   (let [source "(defn read-byte [base length index] (kernel-load-u8 base length index)) (defn main [] 0)"]
     (is (thrown-with-msg?
