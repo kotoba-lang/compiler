1bc4b88ad959c49685afc18872db8312bcb8173d
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-07-23 12:07:28 +0900

    Add bounded condp desugaring (#210)

--- test/kotoba/compiler/test_runner.clj
+++ test/kotoba/compiler/test_runner.clj
@@ -14,6 +14,7 @@
             [kotoba.compiler.document-value-test]
             [kotoba.compiler.string-operation-test]
             [kotoba.compiler.frontend-fuzz-test]
+            [kotoba.compiler.frontend-condp-test]
             [kotoba.compiler.frontend-dotimes-test]
             [kotoba.compiler.f64-value-test]
             [kotoba.compiler.f32-value-test]
@@ -69,6 +70,7 @@
                                           'kotoba.compiler.document-value-test
                                           'kotoba.compiler.string-operation-test
                                           'kotoba.compiler.frontend-fuzz-test
+                                          'kotoba.compiler.frontend-condp-test
                                           'kotoba.compiler.frontend-dotimes-test
                                           'kotoba.compiler.f64-value-test
                                           'kotoba.compiler.f32-value-test
